20e137a481429b66b36d3d5d5a2a78a519c205b8
Patricia Cordero <pfco520@gmail.com>
2024-08-04 22:59:40 -0400

    Setup key and updated env file

--- src/Weather.js
+++ src/Weather.js
@@ -1,29 +1,52 @@
 import React from 'react';
+import { useState } from "react";
 
+const apiKey = process.env.REACT_APP_API_KEY
 function Weather() {
+
+    const [weather, setWeather] = useState({
+        icon: "https://openweathermap.org/img/wn/10d@2x.png",
+        temp: "20",
+        city: "New York City",
+        humidity: "30",
+        speed: "20"
+    })
+
+    function handleSubmit(event) {
+        event.preventDefault()
+
+        let city = event.target.city.value
+
+        if (!city){
+            alert("Please provide the city name")
+            return
+        }
+        fetch()
+    }
+
     return(
         <div className="container my-5">
             <div className="mx-auto rounded border text-center text-white p-4"
                 style={{ backgroundColor: "#3B5FAB", width: "400px" }}>
                     <h2 className="fw-bold mb-5">Weather Forecast</h2>
 
-                    <form className="d-flex mb-3">
-                    <input className="form-control me-2" placeholder="City" aria-label="Search name="city/>
-                    <button className="btn btn-outline-light" type="submit">Search</button>
-                </form>
+                    <form className="d-flex mb-3" onSubmit={handleSubmit}>
+                        <input className="form-control me-2" placeholder="City" name="city" aria-label="Search"/>
+                        <button className="btn btn-outline-light" type="submit">Search</button>
+                     </form>
 
-                <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="..."/>
-                <h1 className="display-4 fw-medium">20°C</h1>
-                <h1 className="mb-5">New York City</h1>
+                <img src={weather.icon} alt="Weather icon"/>
+                <h1 className="display-4 fw-medium">{weather.temp}°F</h1>
+                <h1 className="mb-5">{weather.city}</h1>
 
                 <div className="row mb-3">
                     <div className="col">
                         <i className="bi bi-water"></i> Humidity<br />
-                        40%
+                        {weather.humidity}
                     </div>
                     <div className="col">
                         <i class="bi bi-wind"></i> Wind Speed<br />
-                        23 km/h
+                        {weather.speed}
                     </div>
                 </div>
 
